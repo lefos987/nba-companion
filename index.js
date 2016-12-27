87e604e4a84cdb58fe4189d1eb6b059c4d2a9b60
Lefteris Paraskevas <eparaskevas@salesforce.com>
2016-12-27 23:57:33 +0200

    Add route to verify webhook callback

--- index.js
+++ index.js
@@ -2,11 +2,25 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 
+const PAGE_ACCESS_TOKEN = process.env.NBA_COMPANION_PAGE_ACCESS_TOKEN;
+const WEBHOOK_VERIFY_TOKEN = process.env.NBA_COMPANION_WEBHOOK_VERIFY_TOKEN;
+
 app.set('port', (process.env.PORT || 5000));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
 	res.send({ niki: 'volou' });
+});
+
+app.get('/webhook', (req, res) => {
+	const query = req.query;
+	if (query['hub.mode'] === 'subscribe' && query['hub.verify_token'] === WEBHOOK_VERIFY_TOKEN) {
+		console.log('Validating webhook...');
+		res.status(200).send(query['hub.challenge']);
+	} else {
+		console.error('Failed validation. Make sure the validation tokens match.');
+		res.sendStatus(403);
+	}
 });
 
 app.listen(app.get('port'), function() {
