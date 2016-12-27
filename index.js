f9c2774bb808854f7c86c1c6bd678d7ad8190599
Lefteris Paraskevas <eparaskevas@salesforce.com>
2016-12-28 00:06:43 +0200

    Test connection with FB

--- index.js
+++ index.js
@@ -23,6 +23,46 @@ app.get('/webhook', (req, res) => {
 	}
 });
 
+app.post('/webhook', function (req, res) {
+	var data = req.body;
+	console.log('data', data);
+	// Make sure this is a page subscription
+	// if (data.object == 'page') {
+	// 	// Iterate over each entry
+	// 	// There may be multiple if batched
+	// 	data.entry.forEach(function(pageEntry) {
+	// 		var pageID = pageEntry.id;
+	// 		var timeOfEvent = pageEntry.time;
+	//
+	// 		// Iterate over each messaging event
+	// 		pageEntry.messaging.forEach(function(messagingEvent) {
+	// 			if (messagingEvent.optin) {
+	// 				receivedAuthentication(messagingEvent);
+	// 			} else if (messagingEvent.message) {
+	// 				receivedMessage(messagingEvent);
+	// 			} else if (messagingEvent.delivery) {
+	// 				receivedDeliveryConfirmation(messagingEvent);
+	// 			} else if (messagingEvent.postback) {
+	// 				receivedPostback(messagingEvent);
+	// 			} else if (messagingEvent.read) {
+	// 				receivedMessageRead(messagingEvent);
+	// 			} else if (messagingEvent.account_linking) {
+	// 				receivedAccountLink(messagingEvent);
+	// 			} else {
+	// 				console.log("Webhook received unknown messagingEvent: ", messagingEvent);
+	// 			}
+	// 		});
+	// 	});
+	//
+	// 	// Assume all went well.
+	// 	//
+	// 	// You must send back a 200, within 20 seconds, to let us know you've
+	// 	// successfully received the callback. Otherwise, the request will time out.
+	// 	res.sendStatus(200);
+	// }
+	res.sendStatus(200);
+});
+
 app.listen(app.get('port'), function() {
 	console.log('running on port', app.get('port'))
 });
